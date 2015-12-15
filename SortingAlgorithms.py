eb325e8b225fa7e126f3d9d69c09681be586e529
joeyajames <joeyajames@yahoo.com>
2015-12-14 16:52:30 -0800

    revised InsertionSort

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -13,7 +13,7 @@ threshold = 20
 # not optimized, equiv to while version below, but uses for loop
 def insertion_sort1(A):
 	for i in range(1, len(A)):
-		for j in range(i-1, 0, -1):
+		for j in range(i-1, -1, -1):
 			if A[j] > A[j+1]:
 				A[j], A[j+1] = A[j+1], A[j]
 			else:
