7a2ca08bc3766d152778fca41f861476b30d392f
joeyajames <joeyajames@yahoo.com>
2015-12-15 19:15:38 -0800

    added remove function to Python BST

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -78,37 +78,120 @@ class Tree:
 			return self.root.find(data)
 		else:
 			return False
+	
+	def remove(self, data):
+		# empty tree
+		if self.root is None:
+			return False
 			
+		# data is in root node	
+		elif self.root.value == data:
+			if self.root.leftChild is None and self.root.rightChild is None:
+				self.root = None
+			elif self.root.leftChild and self.root.rightChild is None:
+				self.root = self.root.leftChild
+			elif self.root.leftChild is None and self.root.rightChild:
+				self.root = self.root.rightChild
+			elif self.root.leftChild and self.root.rightChild:
+				delNodeParent = self.root
+				delNode = self.root.rightChild
+				while delNode.leftChild:
+					delNodeParent = delNode
+					delNode = delNode.leftChild
+					
+				self.root.value = delNode.value
+				if delNode.rightChild:
+					if delNodeParent.value > delNode.value:
+						delNodeParent.leftChild = delNode.rightChild
+					elif delNodeParent.value < delNode.value:
+						delNodeParent.rightChild = delNode.rightChild
+				else:
+					if delNode.value < delNodeParent.value:
+						delNodeParent.leftChild = None
+					else:
+						delNodeParent.rightChild = None
+						
+			return True
+		
+		parent = None
+		node = self.root
+		
+		# find node to remove
+		while node and node.value != data:
+			parent = node
+			if data < node.value:
+				node = node.leftChild
+			elif data > node.value:
+				node = node.rightChild
+		
+		# case 1: data not found
+		if node is None or node.value != data:
+			return False
+			
+		# case 2: remove-node has no children
+		elif node.leftChild is None and node.rightChild is None:
+			if data < parent.value:
+				parent.leftChild = None
+			else:
+				parent.rightChild = None
+			return True
+			
+		# case 3: remove-node has left child only
+		elif node.leftChild and node.rightChild is None:
+			if data < parent.value:
+				parent.leftChild = node.leftChild
+			else:
+				parent.rightChild = node.leftChild
+			return True
+			
+		# case 4: remove-node has right child only
+		elif node.leftChild is None and node.rightChild:
+			if data < parent.value:
+				parent.leftChild = node.rightChild
+			else:
+				parent.rightChild = node.rightChild
+			return True
+			
+		# case 5: remove-node has left and right children
+		else:
+			delNodeParent = node
+			delNode = node.rightChild
+			while delNode.leftChild:
+				delNodeParent = delNode
+				delNode = delNode.leftChild
+				
+			node.value = delNode.value
+			if delNode.rightChild:
+				if delNodeParent.value > delNode.value:
+					delNodeParent.leftChild = delNode.rightChild
+				elif delNodeParent.value < delNode.value:
+					delNodeParent.rightChild = delNode.rightChild
+			else:
+				if delNode.value < delNodeParent.value:
+					delNodeParent.leftChild = None
+				else:
+					delNodeParent.rightChild = None
+
 	def preorder(self):
-		print("PreOrder")
-		self.root.preorder()
+		if self.root is not None:
+			print("PreOrder")
+			self.root.preorder()
         
 	def postorder(self):
-		print("PostOrder")
-		self.root.postorder()
+		if self.root is not None:
+			print("PostOrder")
+			self.root.postorder()
 			
 	def inorder(self):
-		print("InOrder")
-		self.root.inorder()
+		if self.root is not None:
+			print("InOrder")
+			self.root.inorder()
 
 bst = Tree()
 print(bst.insert(10))
-bst.insert(5)
-bst.insert(15)
-bst.insert(11)
+
 bst.preorder()
-bst.postorder()
-bst.inorder()
-
-
-
-
-
-
-
-
-
-
-
-
-
+#bst.postorder()
+#bst.inorder()
+print(bst.remove(10))
+bst.preorder()
