23dde35519afa7d96146b6be6dda6cc378ad9292
joeyajames <joeyajames@yahoo.com>
2015-12-21 09:17:50 -0800

    added version 2: get first n primes

--- Primes.py
+++ Primes.py
@@ -1,4 +1,4 @@
-# prime number calculator
+# prime number calculator: find all primes up to n
 
 max = int(input("Find primes up to what number? : "))
 primeList = []
@@ -19,3 +19,28 @@ for x in range(2, max + 1):
 		
 print(primeList)
 		
+		
+#-------------------------------------------------------------
+# prime number calculator: find the first n primes
+
+count = int(input("Find how many primes?: "))
+primeList = []
+x = 2
+
+while len(primeList) < count:
+	isPrime = True
+	index = 0
+	root = int(x ** 0.5) + 1
+	
+	while index < len(primeList) and primeList[index] <= root:
+		if x % primeList[index] == 0:
+			isPrime = False
+			break
+		index += 1
+			
+	if isPrime:
+		primeList.append(x)
+		
+	x += 1
+		
+print(primeList)
