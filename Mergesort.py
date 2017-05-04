406ac60af009d5b33ba6e7200910c33f3b0b1d43
joeyajames <joeyajames@yahoo.com>
2017-05-03 20:38:15 -0700

    revised on 5/3 to fix a bug

--- Mergesort.py
+++ Mergesort.py
@@ -1,12 +1,13 @@
-# revised Mergesort. updated 1/31/2017 based on feedback from users.
-
 import sys
+
+def merge_sort(A):
+	merge_sort2(A, 0, len(A)-1)
 	
-def merge_sort(A, first=0, last=len(A)-1):
+def merge_sort2(A, first, last):
 	if first < last:
 		middle = (first + last)//2
-		merge_sort(A, first, middle)
-		merge_sort(A, middle+1, last)
+		merge_sort2(A, first, middle)
+		merge_sort2(A, middle+1, last)
 		merge(A, first, middle, last)
 		
 def merge(A, first, middle, last):
