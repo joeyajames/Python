3d9a86b03e197faa9b8e4cb21971e8720937367c
Baroudy14-hub <mahmoudismail121@yahoo.com>
2020-05-23 23:42:02 +0200

    Revert "Better implementation of shifting method"
    This reverts commit 43022f6de5ff4c79aaab9a8e048952346e77a37a.

--- Sorting Algorithms/insertion_sort.py
+++ Sorting Algorithms/insertion_sort.py
@@ -22,17 +22,14 @@ def insertion_sort2(A):
 def insertion_sort3(A):
 	for i in range(1, len(A)):
 		curNum = A[i]
-		k = None
-		for j in range(i-1, -1, -1):
+		k = 0
+		for j in range(i-1, -2, -1):
+			k = j
 			if A[j] > curNum:
 				A[j+1] = A[j]
 			else:
-				k = j
 				break
-		if not k:
-			A[0] = curNum
-		else:
-			A[k] = curNum
+		A[k+1] = curNum
 			
 A = [5,9,1,2,4,8,6,3,7]
 print(A)
