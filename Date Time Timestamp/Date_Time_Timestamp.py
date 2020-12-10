fc3a05fd5386794924b9accd98d782af377b864b
Shivam-Singh <sd.shivam.00@gmail.com>
2020-12-10 20:05:29 +0530

    Update Date_Time_Timestamp.py

--- Date Time Timestamp/Date_Time_Timestamp.py
+++ Date Time Timestamp/Date_Time_Timestamp.py
@@ -1,3 +1,4 @@
+
 """##############################################################################
 Using Times, Dates & Timestamps in Python
 
@@ -34,6 +35,74 @@ print(todays_date)
 print(str(todays_date))
 print(todays_date.isoformat())
 todays_date
+
+# different way of displang time 
+
+from datetime import datetime
+now = datetime.now() # current date and time
+
+year = now.strftime("%Y")
+print("year:", year)
+
+month = now.strftime("%m")
+print("month:", month)
+
+day = now.strftime("%d")
+print("day:", day)
+
+time = now.strftime("%H:%M:%S")
+print("time:", time)
+
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+print("date and time:",date_time)
+
+now = datetime.now() # current date and time
+
+year = now.strftime("%Y")
+print("year:", year)
+
+month = now.strftime("%m")
+print("month:", month)
+
+day = now.strftime("%d")
+print("day:", day)
+
+time = now.strftime("%H:%M:%S")
+print("time:", time)
+
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+print("date and time:",date_time)
+
+# different formate of date repersations 
+timestamp = 1528797322
+date_time = datetime.fromtimestamp(timestamp)
+
+print("Date time object:", date_time)
+
+d = date_time.strftime("%m/%d/%Y, %H:%M:%S")
+print("Output 2:", d)	
+
+d = date_time.strftime("%d %b, %Y")
+print("Output 3:", d)
+
+d = date_time.strftime("%d %B, %Y")
+print("Output 4:", d)
+
+d = date_time.strftime("%I%p")
+print("Output 5:", d)
+
+# normal formate 
+timestamp = 1528797322
+date_time = datetime.fromtimestamp(timestamp)
+
+d = date_time.strftime("%c")
+print("Output 1:", d)	
+
+d = date_time.strftime("%x")
+print("Output 2:", d)
+
+d = date_time.strftime("%X")
+print("Output 3:", d)
 
 # Comparison, addition and sutraction of dates
 # ---------------------------
