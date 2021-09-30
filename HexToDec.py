85a18576c0342a542324a1c3cb57100c01461e62
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2021-09-30 22:30:05 +0530

    Update HexToDec.py

--- HexToDec.py
+++ HexToDec.py
@@ -1,5 +1,5 @@
 # Python Hexadecimal to Decimal Conversion
-
+# This is an amazing project
 def __getDecDigit(digit):
 	digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
 		'A', 'B', 'C', 'D', 'E', 'F']
